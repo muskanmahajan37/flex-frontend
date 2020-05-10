3ae2cd20b87199ff8eba1b6f8c5adab6c6cfaaab
Rron <rronjk@gmail.com>
2020-05-10 10:58:39 +0200

    Created a card component which renders some of the categories. Also added images for the card component

--- src/views/HomeScreen.js
+++ src/views/HomeScreen.js
@@ -5,6 +5,7 @@ import "../style/home.css";
 
 // Components
 import Header from "../components/Header";
+import Card from "../components/Card";
 
 const HomeScreen = () => {
   return (
@@ -22,7 +23,34 @@ const HomeScreen = () => {
         <img src={require("../assets/images/img.png")} className="hero-image" />
       </div>
       <div className="second">
-        <p>Second part</p>
+        <p className="services-text">Popular services we ofer:</p>
+        <div className="card-wrapper">
+          <Card
+            cardImage={require("../assets/images/web-design.jpg")}
+            description="Develop Your Website"
+            name="Web Design"
+          />
+          <Card
+            cardImage={require("../assets/images/logo-design.jpg")}
+            description="Build Your Brand"
+            name="Logo Design"
+          />
+          <Card
+            cardImage={require("../assets/images/seo.jpg")}
+            description="Unlock Growth Online"
+            name="SEO"
+          />
+          <Card
+            cardImage={require("../assets/images/translation.jpg")}
+            description="Go Global"
+            name="Translation"
+          />
+          <Card
+            cardImage={require("../assets/images/voice-over.jpg")}
+            description="Share your message"
+            name="Voice Over"
+          />
+        </div>
       </div>
     </div>
   );
