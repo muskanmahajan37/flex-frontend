c815849ec45c31b80ba6977fd087e5e1885e87c0
Rron <rronjk@gmail.com>
2020-05-26 23:07:42 +0200

    Started implementing category header and the styling for it. There's a lot of work to be done

---
+++ src/components/headers/CategoryHeader.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+// Styling
+import "../../style/categoryHeader.css";
+
+const CategoryHeader = () => {
+  return (
+    <ul>
+      <li className="parent-li">
+        Programming
+        <ul className="submenu">
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+        </ul>
+      </li>
+      <li className="parent-li">
+        Design
+        <ul className="submenu">
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+          <li className="subitem">test</li>
+        </ul>
+      </li>
+    </ul>
+  );
+};
+
+export default CategoryHeader;
