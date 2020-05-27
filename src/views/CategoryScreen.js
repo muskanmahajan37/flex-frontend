0be57da8114f4821d0c51533504f9c7de58c3f66
Rron <rronjk@gmail.com>
2020-05-28 00:34:25 +0200

    Started implementing Category and Subcategory screens

---
+++ src/views/CategoryScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Components
+import Header from "../components/headers/Header";
+
+const CategoryScreen = () => {
+  return (
+    <div className="parent">
+      <Header />
+      <h1>Category screen</h1>
+    </div>
+  );
+};
+
+export default CategoryScreen;
---
+++ src/views/SubcategoryScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// Components
+import Header from "../components/headers/Header";
+
+const SubcategoryScreen = () => {
+  return (
+    <div className="parent">
+      <Header />
+      <h1>Subategory screen</h1>
+    </div>
+  );
+};
+
+export default SubcategoryScreen;
