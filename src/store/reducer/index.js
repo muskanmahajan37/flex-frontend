886f7c6439d2e5ac9860b64659cf3293fe004577
Kujtim <kujtim@gildform.com>
2020-05-27 22:48:09 +0200

    Added service reducer and category reducer to RootReducer

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "../auth/reducers";
+import serviceReducer from "../services/reducers";
+import categoryReducer from "../categories/reducers";
 
 export default combineReducers({
   auth: authReducer,
+  services: serviceReducer,
+  categories: categoryReducer,
 });
