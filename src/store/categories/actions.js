3388735d9966d100e805a4b2bf92d84699bf6288
Kujtim <kujtim@gildform.com>
2020-05-27 22:46:50 +0200

    Created category actions, reducers and thunk functions

---
+++ src/store/categories/actions.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_CATEGORIES_REQUEST,
+  FETCH_CATEGORIES_SUCCESS,
+  FETCH_CATEGORIES_FAIL,
+} from "../types";
+
+export const fetchCategoriesRequest = (payload) => ({
+  type: FETCH_CATEGORIES_REQUEST,
+  payload,
+});
+
+export const fetchCategoriesSuccess = (payload) => ({
+  type: FETCH_CATEGORIES_SUCCESS,
+  payload,
+});
+
+export const fetchCategoriesFail = (payload) => ({
+  type: FETCH_CATEGORIES_FAIL,
+  payload,
+});
---
+++ src/store/categories/reducers.js
@@ -0,0 +1,36 @@
+import {
+  FETCH_CATEGORIES_REQUEST,
+  FETCH_CATEGORIES_SUCCESS,
+  FETCH_CATEGORIES_FAIL,
+} from "../types";
+
+const initialState = {
+  categories: [],
+  isLoading: false,
+  loadAgain: true,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CATEGORIES_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case FETCH_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        categories: action.payload,
+        isLoading: false,
+        loadAgain: false,
+      };
+    case FETCH_CATEGORIES_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        loadAgain: true,
+      };
+    default:
+      return state;
+  }
+};
