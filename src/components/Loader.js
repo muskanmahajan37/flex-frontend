edf980e4d636e8fe9be3ad20e06eca5bb4b37a8b
Rron <rronjk@gmail.com>
2020-05-28 22:05:24 +0200

    Created a reusabel loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default ({ msg, format }) => (
+  <div className={`my-loader ${format}`}>
+    <i className="fa fa-circle-o-notch fa-spin"></i>
+    <p>{msg}</p>
+  </div>
+);
