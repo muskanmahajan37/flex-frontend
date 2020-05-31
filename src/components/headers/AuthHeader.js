31db71fa1dfac66a1a3e42ab5fe112b262c25f4a
Rron <rronjk@gmail.com>
2020-05-31 17:00:38 +0200

    Added an image to the header and a menu that activates onClick

--- src/components/headers/AuthHeader.js
+++ src/components/headers/AuthHeader.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 // Components
 import { Navbar, Nav } from "react-bootstrap";
@@ -10,7 +10,23 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../../store/auth/thunks";
 
-const AuthHeader = ({ name, logout }) => {
+const AuthHeader = ({ user, logout }) => {
+  const wrapperRef = useRef(null);
+  const [menu, setMenu] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, false);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, false);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setMenu(false);
+    }
+  };
+
   return (
     <Navbar style={{ padding: 0, paddingTop: "1%" }} className="navbar">
       <Link to="/" className="logo">
@@ -23,17 +39,43 @@ const AuthHeader = ({ name, logout }) => {
         <Link to="/" className="link">
           Switch to selling
         </Link>
-        <Link className="link">{name}</Link>
-        <Link className="link" onClick={() => logout()}>
-          Logout
-        </Link>
+        <div>
+          <img
+            src={`http://localhost:8000/user/${user.image}`}
+            className="profile-image"
+            onClick={() => setMenu(!menu)}
+            onBlur={() => setMenu(false)}
+            ref={wrapperRef}
+          />
+          {menu && (
+            <div className="header-submenu">
+              <Link to="/" className="header-submenu-item">
+                Profile
+              </Link>
+              <Link to="/" className="header-submenu-item">
+                Settings
+              </Link>
+              <Link to="/" className="header-submenu-item">
+                Help & Support
+              </Link>
+              <div className="divider" />
+              <Link
+                to="/"
+                className="header-submenu-item"
+                onClick={() => logout()}
+              >
+                Logout
+              </Link>
+            </div>
+          )}
+        </div>
       </Nav>
     </Navbar>
   );
 };
 
 const mapStateToProps = (state) => ({
-  name: state.auth.user.name,
+  user: state.auth.user,
 });
 
 const mapDispatchToProps = (dispatch) => ({
