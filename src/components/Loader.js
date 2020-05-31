ceb340b3ffdeef8d1f2b60de43ea8d13a2787cc4
Rron <rronjk@gmail.com>
2020-05-31 22:51:40 +0200

    Add color prop to Loader component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default ({ msg, format }) => (
-  <div className={`my-loader ${format}`}>
+export default ({ msg, color, format }) => (
+  <div className={`my-loader ${color} ${format}`}>
     <i className='fa fa-circle-o-notch fa-spin'></i>
     <p>{msg}</p>
   </div>
