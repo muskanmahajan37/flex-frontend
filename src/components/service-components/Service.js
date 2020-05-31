314709c542adc712b852ab9473a752455d51ef37
Rron <rronjk@gmail.com>
2020-05-31 16:59:24 +0200

    Rendering service image

--- src/components/service-components/Service.js
+++ src/components/service-components/Service.js
@@ -3,13 +3,13 @@ import React from "react";
 // Styling
 import "../../style/service.css";
 
-const Service = ({ name, username, price }) => {
+const Service = ({ name, username, price, image }) => {
   return (
     <div className="service-container">
-      <div className="service-image"></div>
+      <img className="service-image" src={`http://localhost:8000/images/${image}`} />
       <p className="service-name">{name}</p>
       <p className="service-description">{username}</p>
-      <p className="service-price">{price}</p>
+      <p className="service-price">€{price}</p>
     </div>
   );
 };
