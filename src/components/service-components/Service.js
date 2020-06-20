8dd8f1ebf3951413409a69e6bc840cf5654977aa
Rron <rronjk@gmail.com>
2020-06-20 18:31:48 +0200

    Added link  to Service screen when clicking over a service. Also fixed the bug where the url had % in url instead of whitespaces, now it has dashes

--- src/components/service-components/Service.js
+++ src/components/service-components/Service.js
@@ -6,22 +6,34 @@ import '../../style/service.css';
 // React-Router
 import { Link } from 'react-router-dom';
 
-const Service = ({ name, username, description, price, image }) => {
+const Service = ({ id, name, username, onClick, price, image }) => {
+  let nameWithDashes = name.replace(/\s+/g, '-').toLowerCase();
+
   return (
-    <div className='service-container'>
+    <Link
+      to={{
+        pathname: `/${username}/${nameWithDashes}`,
+        state: {
+          id,
+          username,
+        },
+      }}
+      onClick={onClick}
+      className='service-container'
+    >
       <img
         className='service-image'
         src={`http://localhost:8000/images/${image}`}
         alt={'Profile'}
       />
-      <div className="service-details-container">
+      <div className='service-details-container'>
         <Link to={`/${username}`} className='user-name'>
           {username}
         </Link>
         <p className='service-name'>{name}</p>
         <p className='service-price'>€{price}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
