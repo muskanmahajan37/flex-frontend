1023f09fe7a03ae8d4902498330b2f63ae3663bb
Rron <rronjk@gmail.com>
2020-06-20 18:30:46 +0200

    Added service screen which shows the single service details

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React from 'react';
 
 // Pages and components
-import HomeScreen from "../views/HomeScreen";
-import LoginScreen from "../views/LoginScreen";
-import RegisterScreen from "../views/RegisterScreen";
-import NotFoundScreen from "../views/NotFoundScreen";
-import CategoryScreen from "../views/CategoryScreen";
-import SubcategoryScreen from "../views/SubcategoryScreen";
-import ProfileScreen from "../views/ProfileScreen";
+import HomeScreen from '../views/HomeScreen';
+import LoginScreen from '../views/LoginScreen';
+import RegisterScreen from '../views/RegisterScreen';
+import NotFoundScreen from '../views/NotFoundScreen';
+import CategoryScreen from '../views/CategoryScreen';
+import SubcategoryScreen from '../views/SubcategoryScreen';
+import ProfileScreen from '../views/ProfileScreen';
+import ServiceScreen from '../views/ServiceScreen';
 
 // React-Router
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={HomeScreen} />
-      <Route path="/login" component={LoginScreen} />
-      <Route path="/register" component={RegisterScreen} />
-      <Route path="/categories/:category" exact component={CategoryScreen} />
+      <Route path='/' exact component={HomeScreen} />
+      <Route path='/login' component={LoginScreen} />
+      <Route path='/register' component={RegisterScreen} />
+      <Route path='/categories/:category' exact component={CategoryScreen} />
       <Route
-        path="/categories/:category/:subcategory"
+        path='/categories/:category/:subcategory'
+        exact
         component={SubcategoryScreen}
       />
-      <Route path="/:user" exact component={ProfileScreen} />
-      <Route path="*" component={NotFoundScreen} />
+      <Route path='/:user' exact component={ProfileScreen} />
+      <Route path='/:user/:service' exact component={ServiceScreen} />
+      <Route path='*' component={NotFoundScreen} />
     </Switch>
   </BrowserRouter>
 );
