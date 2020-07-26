a2045c9f9177caa6ca17d57bea63cd919d1f0ab2
Rron Jakupi <rronjk@gmail.com>
2020-07-26 18:03:13 +0200

    BUILD

---
+++ build/precache-manifest.1fa02f1b647cf501b6dd6470c600bb0d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41eb4a672211d15b6682cf37f13f0ff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1cfec4aff8359f7f237",
+    "url": "/static/css/2.a63652b2.chunk.css"
+  },
+  {
+    "revision": "8935bff6e22613a6d577",
+    "url": "/static/css/main.e6725e8d.chunk.css"
+  },
+  {
+    "revision": "e1cfec4aff8359f7f237",
+    "url": "/static/js/2.c8bcd336.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.c8bcd336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8935bff6e22613a6d577",
+    "url": "/static/js/main.749c94b6.chunk.js"
+  },
+  {
+    "revision": "0ad6712a4f3b8c771e03",
+    "url": "/static/js/runtime-main.ef3a97bc.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "bb21992660a62ea04d8906596f1847a1",
+    "url": "/static/media/img.bb219926.png"
+  }
+]);
